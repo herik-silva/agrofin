74c054d4809c2a3aa4a7bc4cdce20ce5d381e7b7
herik-silva <herik.silva.019@gmail.com>
2023-03-24 14:01:06 -0300

    Updates

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
-        fetch("./service-worker.js", {headers: new Headers({"Content-Type": "application/javascript"})}).then(response => {
-            console.log(response.headers.get("Content-Type"));
-            navigator.serviceWorker.register(response).then(function (registration) {
-                console.log('Service worker registered:', registration);
-            }, function (error) {
-                console.log('Service worker registration failed:', error);
-            });
-        })
+        navigator.serviceWorker.register('/service-worker.js').then(function (registration) {
+            console.log('Service worker registered:', registration);
+        }, function (error) {
+            console.log('Service worker registration failed:', error);
+        });
     });
 }

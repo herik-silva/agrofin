34713de4eaee8697c6a3b103ee80f183791eaa24
herik-silva <herik.silva.019@gmail.com>
2023-03-24 14:12:06 -0300

    Updates

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
-        navigator.serviceWorker.register(`https://herik-silva.github.io/agrofin/service-worker.js`).then(function (registration) {
+        navigator.serviceWorker.register(`https://raw.githubusercontent.com/herik-silva/agrofin/gh-pages/service-worker.js`).then(function (registration) {
             console.log('Service worker registered:', registration);
         }, function (error) {
             console.log('Service worker registration failed:', error);
